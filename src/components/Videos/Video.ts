7c87337393bbe630208a3b7a200ecb38946ea9e7
maxrogers78 <maxrogerssepulveda@gmail.com>
2021-08-06 17:46:14 -0400

    get videos data

---
+++ src/components/Videos/Video.ts
@@ -0,0 +1,8 @@
+export interface IVideo {
+  _id: string;
+  title: string;
+  url: string;
+  description?: string;
+  createdAt: string;
+  updatedAt: string;
+}
--- src/components/Videos/VideoList.tsx
+++ src/components/Videos/VideoList.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { IVideo } from "./Video";
+import * as videoService from "./VideoService";
 
 const VideoList = () => {
+  const [videos, setVideos] = useState<IVideo[]>([]);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const res = await videoService.getVideos();
+    setVideos(res.data);
+  };
+
   return (
     <div>
       <h1>Video List</h1>
+
+      {videos.map((video, idx) => (
+        <p key={idx}>{video.title}</p>
+      ))}
     </div>
   );
 };
